828977b0ccc1320b774ab1e2ce5062795276dd5d
Pranali <pranalilambat00@gmail.com>
2022-02-19 19:15:28 +0530

    UC1_EMP_IS_ABSENT_OR_PRESENT

---
+++ UC_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_By_OOPS
+{
+    
+        class UC1
+        {
+            public void PresentOrNot()
+            {
+                Random random = new Random();
+                int number = random.Next(0,2);
+                {
+                    if (number == 0)
+                    {
+                        Console.WriteLine("Empl is Present");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Empl is Absent");
+                    }
+
+                }
+            }
+        }
+}
