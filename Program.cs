2a96121b31eefbb6c8ae2c8b20d2692e226d2be3
Pranali <pranalilambat00@gmail.com>
2022-02-19 19:09:34 +0530

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Employee_Wage_By_OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Program");
+            //UC1 uc1 = new UC1();
+            //uc1.PresentOrNot();
+
+            //UC_2 uc2 = new UC_2();
+            //uc2.DailyWages();
+
+            //UC3 obj = new UC3();
+            //UC3.Part_Time_Emp1();
+
+            //UC_4 uc4 = new UC_4();
+            // UC_4.EmpWageUsingSwitch();
+
+            //UC5_WageForMonth.WageForMonth();
+
+            //UC6_WageTillNumOfWorkingDays.WageTillNumOfWorkingDays();
+        }
+    }
+}
